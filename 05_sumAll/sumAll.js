cb89ee40e371084fe208c67210b75d59528da11c
Baldo Acerboni <baldo.acerboni@gmail.com>
2023-09-15 10:22:04 +0200

    Solve excercises 05 to 07
    solve excercises so that they pass instruction tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(typeof num1 === 'number' && typeof num2 === 'number') {
+        if(num1 >= 0 && num2 >= 0) {
+            const argsOrdered = [];
+            let rangeSum = 0;
+            
+            if (num1 < num2) {
+                argsOrdered.push(num1);
+                argsOrdered.push(num2);
+            } else {
+                argsOrdered.push(num2);
+                argsOrdered.push(num1);
+            }
+            for(let i = argsOrdered[0]; i <= argsOrdered[1]; i++) {
+                rangeSum += i;
+            }
+            return rangeSum;
 
+        } else {return 'ERROR'}
+    } else {return 'ERROR'}
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  const celsius = parseFloat(((fahr - 32) * 5 / 9).toFixed(1));
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahr = parseFloat((celsius * 9 / 5 + 32).toFixed(1));
+  return fahr;
 };
 
 // Do not edit below this line
