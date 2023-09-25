78680cd8267ae3239daf4f32989923fe09f22a96
Baldo Acerboni <baldo.acerboni@gmail.com>
2023-09-25 11:54:08 +0200

    Finish all assignments

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldest = {}
+    for(let el of arr) {
+        if(el.yearOfDeath) {
+            el.yearsLived = el.yearOfDeath - el.yearOfBirth
+        } else {
+            el.yearsLived = new Date().getFullYear() - el.yearOfBirth
+        }
+        if(!oldest.yearsLived) {
+            oldest = el;
+        } else if(oldest.yearsLived < el.yearsLived) {
+            oldest = el
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
