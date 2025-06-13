30211214462fc5a8e7c377f22e11ddb2bde14b3e
Gnagno <baldo.acerboni@gmail.com>
2025-06-13 12:46:21 +0200

    Fill ex 06 to 11

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  const arr = str.split("");
+  const reverse = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reverse.push(arr[i]);
+  }
+  return reverse.join("");
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let minor;
+  let major;
+  let sum = 0;
+  if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  } else if (a < b) {
+    minor = a;
+    major = b;
+  } else {
+    minor = b;
+    major = a;
+  }
+  for (let i = minor; i <= major; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
